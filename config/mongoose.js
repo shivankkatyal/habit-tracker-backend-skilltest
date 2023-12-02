ef0efbe677ac92d63376e8bfce2602378f5ad743
shivankkatyal <katyalshivank@gmail.com>
2023-12-02 20:46:42 +0530

    Create mongoose.js

---
+++ config/mongoose.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/habit_tracker');
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console,'error connecting to database'));
+
+db.once('open', function(){
+    console.log('Successfully connected to the database');
+});
+
+module.exports = db;
